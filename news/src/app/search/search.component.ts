4b838c761bf68296a5f799145e5fa92f410dcbcf
Julio Wagner <jwagnerhn@outlook.com>
2022-01-27 17:46:32 -0500

    Updated search.component.css to display smaller images.

---
+++ news/src/app/search/search.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  category: string = "";
+  news: any;
+
+  constructor(private http: HttpClient) { }
+
+  findNews(category: string){
+    this.http
+    .get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=1a8aac7473ea4b0196a149454361d807`)
+    .subscribe((response) => {
+      console.log(response);
+      this.news = response;
+    });
+    
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
